0c86ed50b3ea337ad0c5ed5ceb74fa90f37880b5
dnn37 <dnn37@mail.ru>
2015-05-31 16:17:18 +0200

    new changex

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ print('hello world. I love you!\n')
 import sys
 for line in sys.stdin:
     for token in line.strip().split():
-        print(token + '\t1')__author__ = 'DELL'
+        print(token + '\t1')
